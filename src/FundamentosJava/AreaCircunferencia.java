171dbaffd54359bc381eac6f75b18255b06ae702
Lucas <lucaas.daniel2003@gmail.com>
2023-03-03 08:55:13 -0300

    começo do projeto

---
+++ src/FundamentosJava/AreaCircunferencia.java
@@ -0,0 +1,12 @@
+package FundamentosJava;
+
+public class AreaCircunferencia {
+    public static void main(String[] args) {
+        final double pi = 3.14159;
+        double area, raio;
+
+        raio = 10;
+        area = pi * (2*raio);
+        System.out.println(area);
+    }
+}
